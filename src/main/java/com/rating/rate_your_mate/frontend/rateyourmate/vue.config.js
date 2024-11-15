1708e81348eef295c3116151813348a43993ac9e
Tobias Rating <trating3@gmail.com>
2024-11-15 23:32:15 +0100

    Changed workflow

--- src/main/java/com/rating/rate_your_mate/frontend/rateyourmate/vue.config.js
+++ src/main/java/com/rating/rate_your_mate/frontend/rateyourmate/vue.config.js
@@ -1,4 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: '/1Tobu.github.io/'
-})
+module.exports = {
+    publicPath: '/${{ 1Tobu.github.io }}/'
+}
