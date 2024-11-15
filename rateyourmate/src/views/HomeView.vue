49676bbdc5213d951c30fdc8939958c584d26a22
Tobias Rating <trating3@gmail.com>
2024-11-15 17:05:42 +0100

    Added dummy template

---
+++ rateyourmate/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+// @ is an alias to /src
+
+@Options({
+  components: {
+
+  },
+})
+export default class HomeView extends Vue {}
+</script>
