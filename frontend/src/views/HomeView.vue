8138cc33f00d20ab3859736aec70f2862a37ee72
Tobias Rating <trating3@gmail.com>
2024-11-16 01:51:53 +0100

    Added page content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
+  <RecipeItem/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script>
+import RecipeItem from '@/components/RecipeItem.vue'
+export default {
+    components: {
+        RecipeItem
+    },
+    data() {
+        recipes: []
+    }
+}
 </script>
