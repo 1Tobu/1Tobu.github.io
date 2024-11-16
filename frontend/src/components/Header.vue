8da4cade133ce8871fe49a91f0479db4d2d1850a
Tobias Rating <trating3@gmail.com>
2024-11-16 01:50:06 +0100

    Added components

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <input placeholder="Suche...">
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style scoped>
+header {
+    background-color: #F4BC39;
+    height: 100px;
+    padding: 16px;
+    display: flex;
+    align-items: flex-end;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+}
+</style>
